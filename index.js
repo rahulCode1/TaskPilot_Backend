cf6efec646baaa35d623e72a4a211a39a8d627ba
Rahul <pbrkumawat@gmail.com>
2026-01-05 03:08:19 +0530

    fix cors

--- index.js
+++ index.js
@@ -15,7 +15,17 @@ const tagRoute = require("./routes/tag_routes")
 initializeDb()
 
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(cors())
+
+
+
 app.use(express.json())
 
 
@@ -25,8 +35,8 @@ app.use("/api/team", teamRoute)
 app.use("/api/project", projectRoute)
 app.use("/api/tags", tagRoute)
 
-app.use((req, res, next)=>{
-   next(new HttpError("This route doesn't exist.", 404))
+app.use((req, res, next) => {
+    next(new HttpError("This route doesn't exist.", 404))
 })
 
 
